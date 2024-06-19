6856ca142ffc0299fd53d9afaba7d1e3d606203d
Johan <johan@macair2020>
2024-06-19 13:06:56 +0200

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flowbank Syndicate
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const ghUrl = "https://github.com/johnb8005/flowbank-syndicate";
+
+export const version: string = import.meta.env.VITE_VERSION || "unset_version";
+export const sha: string = import.meta.env.VITE_GIT_SHA || "unset_sha";
+export const basename = import.meta.env.BASE_URL;
+
+export const github = {
+  sha: `${ghUrl}/commit/${sha}`,
+  version: `${ghUrl}/releases/tag/${version}`,
+  url: ghUrl,
+};
