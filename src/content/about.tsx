e52dbb6d2eeae14e7c813570fffbbaa5e5098c46
Johan <johan@macair2020>
2024-06-19 13:17:54 +0200

    mv workflows

---
+++ src/content/about.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+
+const About = () => (
+    <section id="about-us" className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">About Us</h2>
+        <p>
+            <strong>Our Mission</strong>
+        </p>
+        <ul className="list-disc ml-8">
+            <li>
+                Gather Information: Collect and share relevant information regarding the
+                bankruptcy proceedings.
+            </li>
+            <li>
+                Unite Efforts: Bring together depositors and stakeholders to amplify our
+                collective voice.
+            </li>
+            <li>
+                Maximize Recovery: Work towards retrieving the highest possible amount
+                of funds for all members.
+            </li>
+        </ul>
+    </section>
+);
+
+export default About
