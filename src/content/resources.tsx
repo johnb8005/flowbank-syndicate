697b3d022a85c5e8b1e6425b19618654fb4c2f2d
Johan <johan@macair2020>
2024-06-19 13:49:04 +0200

    fix resources

--- src/content/resources.tsx
+++ src/content/resources.tsx
@@ -1,24 +1,41 @@
-const Resources = () => (
-    <section id="resources" className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Resources</h2>
-        <ul className="list-disc ml-8">
-            <li>
-                <a href="https://flowbank.com" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Bank website</a>: https://flowbank.com
-            </li>
-            <li>
-                <a href="https://www.pwc.ch/en.html" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Auditor website</a>: https://www.pwc.ch/en.html
-            </li>
-            <li>
-                <a href="https://www.finma.ch/en/news/2024/06/20240613-mm-flowbank/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">FINMA Press Release</a>: https://www.finma.ch/en/news/2024/06/20240613-mm-flowbank/
-            </li>
-            <li>
-                <a href="https://bankingombudsman.ch/fr/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Swiss Banking Ombudsman</a>: https://bankingombudsman.ch/fr/
-            </li>
-            <li>
-                <a href="https://www.zefix.ch/en/search/entity/list/firm/1380191" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Commercial Registrar</a>: https://www.zefix.ch/en/search/entity/list/firm/1380191
-            </li>
-        </ul>
-    </section>
-);
+const content = [
+    ["Bank Website", "https://flowbank.com"],
+    ["Auditor website", "https://www.pwc.ch/en.html"],
+    [
+        "FINMA Press Release",
+        "https://www.finma.ch/en/news/2024/06/20240613-mm-flowbank/",
+    ],
+    ["Swiss Banking Ombudsman", " https://bankingombudsman.ch/fr/"],
+    [
+        "Commercial Register",
+        "https://www.zefix.ch/en/search/entity/list/firm/1380191",
+    ],
+    [
+        "Reddit: FINMA opens bankruptcy proceedings against FlowBank",
+        "https://www.reddit.com/r/SwissPersonalFinance/comments/1devdnf/finma_opens_bankruptcy_proceedings_against/",
+    ],
+];
+
+const Resources = () => {
+    return (
+        <section id="resources" className="mb-8">
+            <h2 className="text-2xl font-bold mb-4">Resources</h2>
+            <ul className="list-disc ml-8">
+                {content.map(([label, link]) => (
+                    <li>
+                        {label}&nbsp;
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-blue-500 hover:underline">
+                            <i className="fa fa-external-link"></i>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
 
 export default Resources;
