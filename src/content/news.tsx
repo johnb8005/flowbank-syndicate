4c2fea45ea5d5d3fe03a41df59d053d6c24d9b25
Johan <johan@macair2020>
2024-06-19 13:58:00 +0200

    end session

--- src/content/news.tsx
+++ src/content/news.tsx
@@ -1,20 +1,32 @@
-const News = () => (
-    <section id="news-updates" className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">News and Updates</h2>
-        <ul>
-            <li>
-                <strong>June 12, 2024:</strong> Swiss Financial Market Supervisory
-                Authority (FINMA) initiated bankruptcy proceedings against FlowBank SA.
-            </li>
-            <li>
-                <strong>June 13, 2024, 08:00 AM:</strong> Bankruptcy proceedings took
-                effect.
-            </li>
-            <li>
-                <strong>June 19, 2024:</strong> Establishment of the Recovery Syndicate.
-            </li>
-        </ul>
-    </section>
-);
+const newsUpdates = [
+    {
+        date: "June 12, 2024",
+        content:
+            "Swiss Financial Market Supervisory Authority (FINMA) initiated bankruptcy proceedings against FlowBank SA.",
+    },
+    {
+        date: "June 13, 2024, 08:00 AM",
+        content: "Bankruptcy proceedings took effect.",
+    },
+    {
+        date: "June 19, 2024",
+        content: "Establishment of the Recovery Syndicate.",
+    },
+];
 
-export default News
+const News = () => {
+    return (
+        <section id="news-updates" className="mb-8">
+            <h2 className="text-2xl font-bold mb-4">News and Updates</h2>
+            <ul>
+                {newsUpdates.map((update, index) => (
+                    <li key={index}>
+                        <strong>{update.date}:</strong> {update.content}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+export default News;
